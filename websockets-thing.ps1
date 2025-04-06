add5b1df22055fc2cb1a8dcb4aafca4af7a3a43f
Paco Delozanne <pdeloz@gmail.com>
2025-04-05 23:32:49 -0500

    Add files via upload

---
+++ websockets-thing.ps1
@@ -0,0 +1,121 @@
+function Parse-Headers
+{
+	Param(
+        $msgBytes
+	)
+
+	$msgString = [System.Text.Encoding]::UTF8.GetString($msgBytes);
+
+	$headerLines = $msgString -split "`r`n";
+
+    $headerDict = @{}
+    foreach ( $line in $headerLines ) {
+        $splitPos = $line.IndexOf(":");
+        if ( $splitPos -lt 0 )
+        {
+            Write-Information "Couldn't parse header line: $line";
+            continue;
+        }
+        $headerName = $line.Substring(0, $splitPos).Trim();
+        $headerValue = $line.Substring($splitPos + 1, $line.Length - $splitPos - 1).Trim();
+        Write-Information "$headerName = $headerValue";
+        $headerDict[$headerName] = $headerValue
+    }
+    $headerDict
+}
+
+function Generate-SecWebSocketAccept
+{
+    Param(
+        $SecWebSocketKey
+    )
+    $SecWebSocketSuffix = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
+    $Bytes = [System.Text.Encoding]::UTF8.GetBytes($SecWebSocketKey + $SecWebSocketSuffix)
+    $Sha1 = [System.Security.Cryptography.SHA1CryptoServiceProvider]::new()
+    $SecWebSocketAccept = [System.Convert]::ToBase64String($Sha1.ComputeHash($Bytes))
+    $SecWebSocketAccept
+}
+
+function Generate-JsonRpcMessage
+{
+    $msgString = "{`"jsonrpc`":`"2.0`",`"id`":0,`"method`":`"getFileNames`",`"params`":{`"server`":`"home`"}}"
+    $msgBytes = [System.Text.Encoding]::UTF8.GetBytes($msgString)
+
+    $header = [byte[]]::new(2)
+    $header[0] = 0x81
+    $header[1] = $msgBytes.Count
+
+    $header + $msgBytes
+}
+
+function Send-JsonRpcMessage
+{
+    Param(
+        [Parameter(Mandatory)] $clientSocket
+    )
+    $msg = Generate-JsonRpcMessage
+    $bytesSent = $clientSocket.Send($msg)
+    Write-Host "Sent $bytesSent bytes for JSON-RPC"
+}
+
+function TestFunc
+{
+    Param(
+        [Parameter(Mandatory)] $listenSocket
+    )
+    $clientSocket = $listenSocket.Accept()
+    $buffer = [byte[]]::new(1024)
+    $bytesReceived = $clientSocket.Receive($buffer)
+    $headers = Parse-Headers $buffer
+
+    # Accept WebSockets Connection
+    $SecWebSocketAccept = Generate-SecWebSocketAccept $headers."Sec-WebSocket-Key"
+    $responseLines = "HTTP/1.1 101 Switching Protocols", "Upgrade: websocket",
+        "Connection: Upgrade", "Sec-WebSocket-Accept: $SecWebSocketAccept", "", ""
+    $responseBytes = [System.Text.Encoding]::UTF8.GetBytes($responseLines -join "`r`n")
+    $bytesSent = $clientSocket.Send($responseBytes)
+    Write-Host "Sent $bytesSent bytes for WebSocket upgrade"
+
+    # JSON-RPC stuff
+    Send-JsonRpcMessage $clientSocket
+
+    # Try to receive response
+    $msg = [String]::Empty
+    while ( $true )
+    {
+        # Get data
+        while ( -not $clientSocket.Poll(1e6, [System.Net.Sockets.SelectMode]::SelectRead) )
+        { <# waiting for data #> }
+        $receiveBuffer = [byte[]]::new($clientSocket.Available)
+        # Write-Host "There are $bytesAvailable bytes available."
+        $bytesReceived = $clientSocket.Receive($receiveBuffer)
+        Write-Host "Received $bytesReceived bytes"
+
+        # parse received websocket msg
+        $flags, $maskedLength = $receiveBuffer[0..1]
+        if ( $flags -ne 0x81 )
+        {
+            Write-Host "Unknown flags: $("0x{0:X}" -f $flags)"
+        }
+        if ( ($maskedLength -band 0x80) -ne 0x80 )
+        {
+            Write-Host "Expected masked but it wasn't"
+        }
+        $maskOffset = switch($maskedLength -band 0x7F)
+        {
+            126 { 4; break }
+            127 { 10; break }
+            default { 2; break }
+        }
+        $maskedKey = $receiveBuffer[$maskOffset..($maskOffset + 3)]
+        Write-Host "maskOffset is $maskOffset"
+        Write-Host "maskedKey is 0x$(($maskedKey | % { "{0:x2}" -f $_ }) -join '')"
+        $unmaskedBuffer = for ( $i = $maskOffset + 4 ; $i -lt $receiveBuffer.Count ; $i++ )
+        {
+            # todo 4 at a time?
+            $receiveBuffer[$i] -bxor $maskedKey[$i % 4]
+        }
+        $msg += [System.Text.Encoding]::UTF8.GetString($unmaskedBuffer)
+        $msg
+    }
+}
